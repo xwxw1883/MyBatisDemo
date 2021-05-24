8e95d037076aed064f891ec0bc0faff78606ad92
xw <731519876@qq.com>
2021-05-24 09:43:27 +0800

    "mybatis测试工程2"

--- myBatis-01/src/main/java/dao/UserMapper.java
+++ myBatis-01/src/main/java/dao/UserMapper.java
@@ -3,14 +3,18 @@ package dao;
 import pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     //获取全部用户
     List<User> getUserList();
+    //获取全部用户
+    List<User> getUserLike(String value);
 //    根据id查询用户
     User getUserById(int i);
     //新增一个用户
     int addUser(User user);
+    int addUser2(Map<String,Object> map);
     //修改一个客户
     int upDateUser(User user);
     int deleteUser(int id);
--- myBatis-01/src/test/java/dao/DemoTest.java
+++ myBatis-01/src/test/java/dao/DemoTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import pojo.User;
 import until.MyBatisUntil;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DemoTest {
     @Test
@@ -53,12 +55,38 @@ public class DemoTest {
         sqlSession.commit();
         sqlSession.close();
     }
+    //用map传递
+    @Test
+    public void addUser2(){
+        SqlSession sqlSession = MyBatisUntil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<String,Object> ();
+        map.put ("userid",6);
+        map.put ("username","1234");
+        map.put ("userpwd","1234");
+        int hhh = mapper.addUser2(map);
+        System.out.println(hhh);
+        //JDBC 提交事物 增删改查
+        sqlSession.commit();
+        sqlSession.close();
+    }
     @Test
     public  void deleteUser(){
         SqlSession sqlSession = MyBatisUntil.getSqlSession();
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         int i = mapper.deleteUser(1);
         System.out.println(i);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public  void getUserLike(){
+        SqlSession sqlSession = MyBatisUntil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> x = mapper.getUserLike ("X");
+        for (User user : x) {
+            System.out.println (user);
+        }
         sqlSession.commit();
         sqlSession.close();
     }
